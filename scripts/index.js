c14c3d11007fa6e9b4c58c3a447552722e88c09c
Adhyuth Narayan <adhyuthnarayan008@gmail.com>
2023-09-15 02:04:27 +0530

    Cool heading

--- scripts/index.js
+++ scripts/index.js
@@ -7,8 +7,8 @@ function ScrollDown() {
     );
 }
 
-// function ChangeBG_Color() {
-// }
-
-
+function ChangeBG_Color() {
+    document.body.style.cssText = 'background-image: radial-gradient(#000000 60%, #8e8e8e)';
+    document.getElementById('heading').style.cssText = 'color: #dbdbdb';
+}
 
