410da0b4a8a6dd354418f4a505d88440effbd4e7
Adhyuth Narayan <adhyuthnarayan008@gmail.com>
2023-10-14 17:58:27 +0530

    lol

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,7 @@
 let heading_state = 0;
 let myname = document.getElementById('heading');
-let pane1 = document.getElementsByClassName("pane1");
-let pane2 = document.getElementsByClassName("pane2");
+let pane_todo = document.getElementsByClassName("pane_todo");
+let pane_internet = document.getElementsByClassName("pane_internet");
 
 function ScrollDown() {
     window.scrollTo(
@@ -18,22 +18,22 @@ function ChangeBG_Color() {
         document.body.style.cssText = 'background-image: radial-gradient(#000000 60%, #8e8e8e)';
         document.getElementById('heading').style.cssText = 'color: #dbdbdb';
         document.getElementById('beautiful_internet').style.cssText = 'color: #dbdbdb';
-        for (i = 0; i < pane1.length; i++){
-            pane1[i].style.cssText = 'color: #000000; background-color: #dbdbdb';
+        for (i = 0; i < pane_todo.length; i++){
+            pane_todo[i].style.cssText = 'color: #000000; background-color: #dbdbdb';
         }
-        for (i = 0; i < pane2.length; i++){
-            pane2[i].style.cssText = 'background-color: #000000';
+        for (i = 0; i < pane_internet.length; i++){
+            pane_internet[i].style.cssText = 'background-color: #000000';
         }
     }
     else{
         document.body.style.cssText = 'background-image: radial-gradient(#b6a0ff, #1a1425 80%)';
         document.getElementById('heading').style.cssText = 'color: #b6a0ff';
         document.getElementById('beautiful_internet').style.cssText = 'color: #1a1425';
-        for (i = 0; i < pane1.length; i++){
-            pane1[i].style.cssText = 'color: #d6c9ff; background-color: #1a1425';
+        for (i = 0; i < pane_todo.length; i++){
+            pane_todo[i].style.cssText = 'color: #d6c9ff; background-color: #1a1425';
         }
-        for (i = 0; i < pane2.length; i++){
-            pane2[i].style.cssText = 'background-color: #b6a0ff';
+        for (i = 0; i < pane_internet.length; i++){
+            pane_internet[i].style.cssText = 'background-color: #b6a0ff';
         }
     }
 }
