c4a330560e22eb04c1c17570d7515c41c60afda1
evoprim3 <adhyuthnarayan008@gmail.com>
2023-12-02 20:59:07 +0530

    made JS modular

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,7 @@
 let heading_state = 0;
-let myname = document.getElementById('heading');
+let email_state = 0;
 let pane_todo = document.getElementsByClassName("pane_todo");
-let pane_internet = document.getElementsByClassName("pane_internet");
+var root_css = document.querySelector(':root');
 
 function ScrollDown() {
     pane_todo[0].scrollIntoView({ behavior: 'smooth' });
@@ -10,30 +10,26 @@ function ScrollDown() {
 function ChangeBG_Color() {
     heading_state = !Boolean(heading_state);
     if (heading_state == 1){
-        document.body.style.cssText = 'background-image: radial-gradient(#000000 60%, #8e8e8e)';
-        document.getElementById('heading').style.cssText = 'color: #dbdbdb';
-        document.getElementById('beautiful_internet').style.cssText = 'color: #dbdbdb';
-        for (i = 0; i < pane_todo.length; i++){
-            pane_todo[i].style.cssText = 'color: #000000; background-color: #dbdbdb';
-        }
-        for (i = 0; i < pane_internet.length; i++){
-            pane_internet[i].style.cssText = 'background-color: #000000';
-        }
+        root_css.style.setProperty('--ui-primary-light', '#e9e9e9');
+        root_css.style.setProperty('--ui-primary-dark', '#000000');
+        root_css.style.setProperty('--text-primary', '#ffffff');
+        root_css.style.setProperty('--text-shadow', '#ffffff');
     }
     else{
-        document.body.style.cssText = 'background-image: radial-gradient(#b6a0ff, #1a1425 80%)';
-        document.getElementById('heading').style.cssText = 'color: #b6a0ff';
-        document.getElementById('beautiful_internet').style.cssText = 'color: #1a1425';
-        for (i = 0; i < pane_todo.length; i++){
-            pane_todo[i].style.cssText = 'color: #d6c9ff; background-color: #1a1425';
-        }
-        for (i = 0; i < pane_internet.length; i++){
-            pane_internet[i].style.cssText = 'background-color: #b6a0ff';
-        }
+        root_css.style.setProperty('--ui-primary-light', '#b6a0ff');
+        root_css.style.setProperty('--ui-primary-dark', '#1a1425');
+        root_css.style.setProperty('--text-primary', '#b6a0ff');
+        root_css.style.setProperty('--text-shadow', '#d1c7f1');
     }
 }
 
 function displayMail(){
-    document.getElementById('email').style.cssText = 'visibility: visible';
+    email_state = !Boolean(email_state);
+    if (email_state == 1){
+        document.getElementById('email').style.cssText = 'visibility: visible';
+    }
+    else {
+        document.getElementById('email').style.cssText = 'visibility: hidden';
+    }
 }
 
