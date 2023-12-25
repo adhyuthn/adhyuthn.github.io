998448529a20e4345c95c74d7f2bf901dcf9a11b
evoprim3 <adhyuthnarayan008@gmail.com>
2023-12-25 23:43:51 +0530

    Send help (2),,,,

--- scripts/base.js
+++ scripts/base.js
@@ -3,6 +3,8 @@ let body_element = document.getElementsByTagName("body");
 let easter_count = 0;
 var root_css = document.querySelector(':root');
 
+console.log("base.js imported")
+
 const theme_state = localStorage.getItem('theme_state');
 Set_theme(theme_state)
 
@@ -28,6 +30,7 @@ function ChangeBG_Color() {
     heading_state = !Boolean(heading_state);
     console.log(easter_count);
     localStorage.setItem('theme_state', heading_state.toString());
+    console.log("theme_state set to: ", theme_state)
     if (easter_count == 10) {
         body_element[0].style.cssText = 'background-image: linear-gradient(90deg, rgba(255,0,0,1) 0%, rgba(255,154,0,1) 10%, rgba(208,222,33,1) 20%, rgba(79,220,74,1) 30%, rgba(63,218,216,1) 40%, rgba(47,201,226,1) 50%, rgba(28,127,238,1) 60%, rgba(95,21,242,1) 70%, rgba(186,12,248,1) 80%, rgba(251,7,217,1) 90%, rgba(255,0,0,1) 100%);';
         console.log("easter egg unlocked");
--- scripts/index.js
+++ scripts/index.js
@@ -1,14 +1,13 @@
-import './base';
+import './base.js';
 
 let email_state = 0;
 let pane_todo = document.getElementsByClassName("pane_todo");
+let scroll_button = document.getElementById("scroll_down");
+let heading = document.getElementsByClassName("heading")[0];
+let email = document.getElementById("email_button");
 
 function ScrollDown() {
     pane_todo[0].scrollIntoView({ behavior: 'smooth' });
-}
-
-function hacker_text() {
-    
 }
 
 function displayMail(){
@@ -21,3 +20,6 @@ function displayMail(){
     }
 }
 
+scroll_button.addEventListener("click", ScrollDown());
+heading.addEventListener("click", ChangeBG_Color())
+email.addEventListener("click", displayMail());
