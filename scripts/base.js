26a5c93b8ae0700792734873d9c5843290c6214e
adhyuthn <adhyuthnarayan008@gmail.com>
2024-11-20 22:04:27 +0530

    more messy JS

--- scripts/base.js
+++ scripts/base.js
@@ -1,7 +1,7 @@
 let heading_state = 0;
 let body_element = document.getElementsByTagName("body");
 let easter_count = 0;
-var root_css = document.querySelector(':root');
+let root_css = document.querySelector(':root');
 
 console.log("base.js imported");
 
@@ -29,14 +29,15 @@ function Set_theme(state) {
     }
 }
     
-easter_count++;
 export function ChangeBG_Color() {
     heading_state= !Boolean(heading_state);
     console.log(easter_count);
     localStorage.setItem('theme_state', heading_state.toString());
+    easter_count++;
     if (easter_count == 10) {
-        body_element[0].style.cssText = 'background-image: linear-gradient(90deg, rgba(255,0,0,1) 0%, rgba(255,154,0,1) 10%, rgba(208,222,33,1) 20%, rgba(79,220,74,1) 30%, rgba(63,218,216,1) 40%, rgba(47,201,226,1) 50%, rgba(28,127,238,1) 60%, rgba(95,21,242,1) 70%, rgba(186,12,248,1) 80%, rgba(251,7,217,1) 90%, rgba(255,0,0,1) 100%);';
+        body_element[0].style.cssText = 'background-image: linear-gradient(90deg, rgba(255,0,0,1) 0%, rgba(255,154,0,1) 10%,rgba(208,222,33,1) 20%, rgba(79,220,74,1) 30%, rgba(63,218,216,1) 40%, rgba(47,201,226,1) 50%, rgba(28,127,238,1) 60%, rgba(95,21,242,1) 70%, rgba(186,12,248,1) 80%, rgba(251,7,217,1) 90%, rgba(255,0,0,1) 100%);';
         console.log("easter egg unlocked");
+        document.getElementById("name").textContent = "IT'S TOO COLORFUL."
     }
     if (easter_count < 10){
         console.log("Going to set theme");
--- scripts/index.js
+++ scripts/index.js
@@ -5,6 +5,8 @@ let pane_about = document.getElementsByClassName("pane_about");
 let scroll_button = document.getElementById("scroll_down");
 let heading = document.getElementsByClassName("heading")[0];
 let email = document.getElementById("email_button");
+let beautiful_internet = document.getElementById("beautiful_internet")
+const beautiful_internet_text = beautiful_internet.innerText
 
 function ScrollDown() {
     pane_about[0].scrollIntoView({ behavior: 'smooth' });
@@ -20,6 +22,35 @@ function displayMail(){
     }
 }
 
+function glitchText() {
+    let counter = 0;
+    const iterations = beautiful_internet_text.length;
+    console.log(`iterations: ${iterations}`);
+    let intervalID = setInterval(() => {
+        beautiful_internet.innerText = beautiful_internet.innerText.split("").map(
+            (element, index) => { 
+                if (index < counter) {
+                    return beautiful_internet_text[index];
+                }
+                return String.fromCharCode(65 + Math.floor(Math.random() * 26));
+            }
+        ).join('');
+        if (counter > iterations) {
+            clearInterval(intervalID);
+        }
+        counter += 1/2;
+    }, 20);
+}
+
+const beautiful_internet_observer_opts = {
+    root: null,
+    rootMargin: "0px",
+    threshold: 1.0,
+};
+const beautiful_internet_observer = new IntersectionObserver(glitchText, beautiful_internet_observer_opts);
+
+beautiful_internet_observer.observe(beautiful_internet);
 scroll_button.addEventListener("click", ScrollDown);
 heading.addEventListener("click", base.ChangeBG_Color)
 email.addEventListener("click", displayMail);
+beautiful_internet.addEventListener("mouseover", glitchText);
