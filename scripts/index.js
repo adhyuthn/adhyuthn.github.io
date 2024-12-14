11fdb1de618623e3a01ab1c4d4f631dae2d91f10
adhyuthn <adhyuthnarayan008@gmail.com>
2024-12-14 12:58:38 +0530

    delete

--- scripts/index.js
+++ scripts/index.js
@@ -7,7 +7,6 @@ let heading = document.getElementsByClassName("heading")[0];
 let email = document.getElementById("email_button");
 let beautiful_internet = document.getElementById("beautiful_internet")
 let beautiful_internet_pane = document.getElementsByClassName("pane_internet")[0];
-let notif_toast = document.getElementById("notif_toast")
 const beautiful_internet_text = beautiful_internet.innerText
 
 function ScrollDown() {
